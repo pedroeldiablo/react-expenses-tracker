75fb3561c5161e600ea9e3ea1fead5bd64b10487
Peter Williams <pedroeldiablo@yahoo.com>
2020-04-29 18:43:57 +0100

    following reducer pattern add and remove transactions

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState'
 
 export const Transaction = ({transaction}) => {
     const sign = transaction.amount < 0 ? '-' : '+'
+    const { deleteTransaction } = useContext(GlobalContext);
     return (
         <div>
              <li className={transaction.amount < 0 ? 'minus' : 'plus'} key={transaction.id}>
-                {transaction.text} <span>{sign}£{Math.abs(transaction.amount)}</span><button className="delete-btn">x</button>
+                {transaction.text} <span>{sign}£{Math.abs(transaction.amount)}</span><button className="delete-btn" onClick={() => deleteTransaction(transaction.id)}>x</button>
             </li>
             
         </div>
--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,5 +1,16 @@
-export const AppReducer = ({state, action} ) => {
+export const AppReducer = (state, action) => {
+    console.log();
     switch(action.type){
+        case  'DELETE_TRANSACTION':
+            return {
+                ...state, 
+                transactions: state.transactions.filter(transaction => transaction.id !== action.payload)
+            }
+            case 'ADD_TRANSACTION':
+                return {
+                    ...state,
+                    transactions: [action.payload, ...state.transactions]
+                }
         default: 
         return state;
     }
