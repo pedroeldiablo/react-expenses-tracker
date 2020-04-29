dae4dd8e5937153d738e2003e9cf731cf93f2b8f
Peter Williams <pedroeldiablo@yahoo.com>
2020-04-29 17:07:59 +0100

    adding component level state for controlled form

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,20 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const AddTransaction = () => {
+    const [text, setText] = useState('');
+    const [amount, setAmount] = useState(0);
     return (
         <>
         <h3>Add transaction</h3>
         <form id="form">
             <div className="form-control">
                 <label htmlFor="text">Text</label>
-                <input type="text" id="text" placeholder="Enter text..."/>
+                <input type="text" id="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter text..."/>
             </div>
             <div className="form-control">
                 <label htmlFor="amount">
                     Amount <br />
                     (negative - expense, positive - income)
                 </label>
-                <input type="number" id="amount" placeholder="Enter amount..." />
+                <input type="number" id="amount" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Enter amount..." />
             </div>
         </form>
             
